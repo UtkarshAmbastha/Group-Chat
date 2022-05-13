9d318de32d05e6d69b55cbdce422fda7499f1083
Utkarsh Ambastha <utkarsh.ambastha@knoldus.com>
2022-05-13 11:50:21 +0530

    GroupChat Backup Option

---
+++ src/test/java/com/knoldus/groupChat/GroupChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoldus.groupChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
